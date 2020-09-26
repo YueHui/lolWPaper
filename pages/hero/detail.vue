da27a7a62a4c157020648dafeec9bce00baba075
YueHui <527329923@qq.com>
2020-09-26 16:57:04 +0800

    up

---
+++ pages/hero/detail.vue
@@ -0,0 +1,131 @@
+
+<style lang="less" scoped>
+	canvas{width: 100vw; height:100vh; background: #000;}
+	button{position: absolute; bottom: 0; left: 0;}
+</style>
+
+<template>
+	<view id="contain">
+		<canvas canvas-id="canvas" :prop="bigSrc" :change:prop="stage.showImage" disable-scroll ></canvas>
+		<button type="default" @click="goBack">返回</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		onLoad() {
+			this.bigSrc = uni.getStorageSync('bigSrc');
+			console.log(this.bigSrc);
+		},
+		data() {
+			return {
+				bigSrc:''
+			};
+		},
+		methods:{
+			goBack(){
+				uni.navigateBack({
+					
+				})
+			}
+		}
+	}
+</script>
+
+<script module="stage" lang="renderjs">
+	let stage,img;
+	
+	export default {
+		data(){
+			return {
+				show:false
+			}
+		},
+		mounted() {
+			const script = document.createElement('script')
+			// view 层的页面运行在 www 根目录，其相对路径相对于 www 计算
+			
+			script.onload = loadEaseljs.bind(this);
+			script.src = 'static/hammer.min.js';
+			document.head.append(script);
+			
+			function loadEaseljs(){
+				const script = document.createElement('script')
+				// view 层的页面运行在 www 根目录，其相对路径相对于 www 计算
+				
+				script.onload = async ()=>{
+					
+					stage = new createjs.Stage(document.getElementsByTagName('canvas')[0]);
+					stage.enableMouseOver();
+					const bigImg = await this.loadImg(this.bigSrc);
+					img = new createjs.Bitmap(bigImg);
+					img.regX = bigImg.width/2;
+					img.regY = bigImg.height/2;
+					img.x = 0;
+					img.y = 0;
+					img.startX = 0;
+					img.startY = 0;
+					stage.addChild(img);
+					stage.update();
+					this.initHammer();
+				}
+				script.src = 'static/easeljs.min.js';
+				document.head.append(script);
+			}
+		},
+		methods:{
+			initHammer(){
+				console.log("init");
+				const contain = document.getElementById('contain');
+				const hammer = new Hammer(contain);
+				console.log({
+					regx:img.regX,
+					regy:img.regY,
+					x:img.x,
+					y:img.y
+				});
+				hammer.on('pan',function(e){
+					
+					console.log({
+						x:img.x,
+						y:img.y,
+						deltaX:e.deltaX,
+						deltaY:e.deltaY
+					});
+					
+					
+					img.x = img.startX + e.deltaX;
+					img.y = img.startY + e.deltaY;
+					stage.update();
+					if(e.isFinal){
+						img.startX = img.x;
+						img.startY = img.y;
+					}
+					
+				})
+				
+				hammer.on('pinch',function(e){
+					console.log(e.scale);
+					img.scaleX = img.scaleY = e.scale;
+				})
+			},
+			loadImg(url){
+				return new Promise((resolve,reject)=>{
+					const image = new Image();
+					image.onload=function(){
+						resolve(image);
+					}
+					image.onerror=function(e){
+						console.log(e);
+						reject();
+					}
+					image.src=url;
+				})
+			}
+			
+		}
+	}
+	
+</script>
+
+
--- pages/hero/hero.vue
+++ pages/hero/hero.vue
@@ -5,11 +5,7 @@
 	.heroInfo{padding: 20px;}
 	.skinItem{text-align: center;}
 	.skinTit{text-align: center;}
-	.bigImg{
-		width: 100vw; height: 100vh; position: fixed; top: 0; left:0; display: none;
-		canvas{width: 100%; height:100%; background: #000;}
-		button{position: absolute; bottom: 0; left: 0;}
-	}
+	
 </style>
 
 <template>
@@ -19,13 +15,9 @@
 			<view>{{hero.name}}</view>
 			<view>{{hero.shortBio}}</view>
 			<view v-for="skin in skins" class="skinItem">
-				<image :id="skin.mainImg" :src="skin.mainImg || skin.chromaImg" mode="widthFix" @click="stage.showImage(skin.mainImg)"></image>
+				<image :id="skin.mainImg" :src="skin.mainImg || skin.chromaImg" mode="widthFix" @click="showBig(skin.mainImg)"></image>
 				<view class="skinTit">{{skin.name}}</view>
 			</view>
-		</view>
-		<view class="bigImg" id="bigImg">
-			<canvas  canvas-id="canvas" id="canvas"></canvas>
-			<button type="default" button-id="closeBtn">关闭</button>
 		</view>
 	</view>
 </template>
@@ -43,7 +35,6 @@
 				hero:{},
 				skins:[],
 				showBigImg:false,
-				bigSrc:''
 			};
 		},
 		watch:{
@@ -75,74 +66,12 @@
 				return this.skins[0].sourceImg
 			},
 			showBig(url){
-				this.showBigImg = true;
-				this.bigSrc = url;
+				uni.setStorageSync("bigSrc",url);
+				uni.navigateTo({
+					url:`/pages/hero/detail`
+				})
 			},
-			closeBigImg(){
-				this.showBigImg = false;
-				this.bigSrc = '';
-			}
 		}
 	}
 </script>
 
-<script module="stage" lang="renderjs">
-	let stage;
-	export default {
-		mounted() {
-			const script = document.createElement('script')
-			// view 层的页面运行在 www 根目录，其相对路径相对于 www 计算
-			
-			script.onload = ()=>{
-				// this.initHammer();
-			}
-			script.src = 'static/hammer.min.js';
-			document.head.append(script);
-			
-			
-		},
-		methods:{
-			initHammer(){
-				const hammer = new Hammer(document.getElementById('canvas'));
-				hammer.on('pan',function(e){
-					console.log(e);
-				})
-			},
-			loadImg(url){
-				return new Promise((resolve,reject)=>{
-					const image = new Image();
-					image.onload=function(){
-						resolve(image);
-					}
-					image.onerror=function(e){
-						console.log(e);
-						reject();
-					}
-					image.src=url;
-				})
-				
-			},
-			closeBigImg(){
-				console.log('close');
-				document.getElementById("bigImg").style.display = "none";
-			},
-			async showImage(url){
-				console.log("showImage");
-				document.getElementById("bigImg").style.display = "block";
-				const image = await this.loadImg(url);
-				const canvas = document.getElementsByTagName('canvas')[0];
-				
-				const ctx = canvas.getContext('2d');
-				ctx.drawImage(image,0,0);
-				
-				this.initHammer();
-				
-				document.querySelector('[button-id=closeBtn]').click=()=>{
-					console.log('close');
-					this.closeBigImg()
-				}
-			},
-			
-		}
-	}
-</script>
